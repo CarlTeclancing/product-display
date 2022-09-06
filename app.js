d02e32f5a182ec79bd514e01c267bce5bbe5a6c5
YuvCarl <carldrake969@gmail.com>
2022-09-06 05:03:46 +0100

    updated on slider button

--- app.js
+++ app.js
@@ -1,6 +1,38 @@
 let closeNav = document.getElementById('close');
 let openNav = document.getElementById('open');
+let img1 = document.getElementById('img1');
+let img2 = document.getElementById('img2');
+let img3 = document.getElementById('img3');
+let img4 = document.getElementById('img4');
 
+
+img1.addEventListener('click', function(){
+   document.getElementById('main-img1').style.display = 'block';
+   document.getElementById('main-img2').style.display = 'none';
+   document.getElementById('main-img3').style.display = 'none';
+   document.getElementById('main-img4').style.display = 'none';
+})
+
+img2.addEventListener('click', function(){
+    document.getElementById('main-img1').style.display = 'none';
+    document.getElementById('main-img2').style.display = 'block';
+    document.getElementById('main-img3').style.display = 'none';
+    document.getElementById('main-img4').style.display = 'none';
+ })
+
+ img3.addEventListener('click', function(){
+    document.getElementById('main-img1').style.display = 'none';
+    document.getElementById('main-img2').style.display = 'none';
+    document.getElementById('main-img3').style.display = 'block';
+    document.getElementById('main-img4').style.display = 'none';
+ })
+
+ img4.addEventListener('click', function(){
+    document.getElementById('main-img1').style.display = 'none';
+    document.getElementById('main-img2').style.display = 'none';
+    document.getElementById('main-img3').style.display = 'none';
+    document.getElementById('main-img4').style.display = 'block';
+ })
 
 closeNav.addEventListener('click', function(){
     
@@ -22,3 +54,7 @@ openNav.addEventListener('click', function(){
     
 
 });
+
+
+
+
